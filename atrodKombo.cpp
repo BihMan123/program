f0be7a496b469b51dcdc850738e07da1059fcbd8
BihMan123 <87981371+BihMan123@users.noreply.github.com>
2025-12-03 09:06:02 +0200

    Update atrodKombo.cpp

--- atrodKombo.cpp
+++ atrodKombo.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
+auto start = high_resolution_clock::now();
 
 bool vaipildas(int a, int b, int c)
 {
@@ -12,24 +15,41 @@ bool vaipildas(int a, int b, int c)
     return false;
 }
 
+struct Trijnieks {
+int a, b, c;   
+};
 
 int main()
 {
+    vector<Trijnieks> atrastie;
     int a;
     int b;
     int c;
-    cout<<"nih ievadi 3 skaitlus, c lielaks par b, b lielaks par a\nIevadiet a:";
-    cin>>a;
-    cout<<"ievadiet b:";
-    cin>>b;
-    cout<<"ievadiet c:";
-    cin>>c;
-    if(vaipildas(a, b, c)==true)
+    int n = 20;
+for(int i = 1; i<=n; i++)
     {
-        cout<<"derīgs kombo";
-    }else
-    {
-        cout<<"mirsti";
+    a = i;
+    for(int i = a; i<=n; i++)
+        {
+        b = i;
+        c=a*a+b;
+        if(c<=n)
+            {
+            if(vaipildas(a, b, c)==true)
+                {
+                    
+                    atrastie.push_back({a, b, c});
+                }
+            }
+        
+        }
     }
+    for(int i = 0; i<atrastie.size(); i++)
+    {
+        cout<<atrastie[i].a<<" "<<atrastie[i].b<<" "<<atrastie[i].c<<endl;
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    cout << duration.count() << endl;
     
 }
